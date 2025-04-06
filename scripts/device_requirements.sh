c793b0d5a7f93f48f86d07670a0e7f073f056e80
Liam <belowzeroiq@proton.me>
2025-04-06 07:16:22 -0700

    update set_requirements function to remove existing vendor directories before cloning

--- scripts/device_requirements.sh
+++ scripts/device_requirements.sh
@@ -2,7 +2,9 @@
 
 set_requirements() {
 rm -rf vendor/arrow
+rm -rf vendor/qcom/opensource/display
 git clone --depth=1 https://github.com/passive-development/android_vendor_arrow -b arrow-13.1 vendor/arrow
+git clone --depth=1 https://github.com/passive-development/android_vendor_qcom_opensource_display -b lineage-20.0 vendor/qcom/opensource/display
 }
 
 set_requirements
